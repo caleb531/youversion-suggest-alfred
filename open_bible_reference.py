482c9a7698609541368f6b95979abde4dcc4f06e
Caleb Evans <caleb@calebevans.me>
2015-01-02 21:07:15 -0800

    Refactored much of the codebase

--- open_bible_reference.py
+++ open_bible_reference.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Open bible reference on YouVersion website
 
 import webbrowser
--- search_bible_reference.py
+++ search_bible_reference.py
@@ -1,16 +1,15 @@
+#!/usr/bin/env python
 # Search bible references corresponding to the typed query
 
-# Import print function
-from __future__ import print_function
 # Import required modules
-import cgi, json, re, time, urllib
+import cgi, json, re, time
 
 # Base class for creating objects of attributes
 class AttrObject:
-	def __init__(self, attrs=None):
-		if attrs:
-			for key, value in attrs.items():
-				setattr(self, key, value)
+    def __init__(self, attrs=None):
+        if attrs:
+            for key, value in attrs.items():
+                setattr(self, key, value)
 
 # Subclasses used for data storage
 class Book(AttrObject): pass
@@ -19,186 +18,190 @@ class Reference(AttrObject): pass
 
 # Load in books of the Bible
 with open('books.json', 'r') as file:
-	books = tuple(Book(book) for book in json.loads(file.read()))
+    books = tuple(Book(book) for book in json.loads(file.read()))
 
-# Load in Bible translations
+# Load in Bible versions (translations)
 with open('versions.json', 'r') as file:
-	all_versions = tuple(json.loads(file.read()))
+    all_versions = tuple(json.loads(file.read()))
+
+# Load in XML <item> template
+with open('item.xml', 'r') as file:
+    item_xml = file.read()
 
 # Default translation for all results
 default_version = 'NIV'
 
-# Base query URL
-bible_url = 'https://www.bible.com/bible'
-search_url = 'https://www.bible.com/search/bible'
+# Base bible reference URL
+base_url = 'https://www.bible.com/bible'
 
-# Regular expression for parsing a bible reference
-bible_ref_patt = '(^((\d+ )?[a-z ]+)( (\d+)(\:(\d+)?)?)?( [a-z\d]+)?)$'
+# Pattern for parsing any bible reference
+bible_ref_patt = '^((\d+ )?[a-z ]+)( (\d+)((\:|\.)(\d+)?)?)?( [a-z\d]+)?$'
+# Pattern for parsing a chapter:verse reference (irrespective of book)
+chapter_dot_verse_patt = '(\d+)\.(\d+)'
 
-# Guess a translation based on the given text
-def guess_version(text):
-		text = text.upper()
+# Guess a translation based on the given partial version
+def guess_version(partial_version):
+    partial_version = partial_version.upper()
 
-		if text in all_versions:
-			version_guess = text
-		else:
-			# Use a predetermined version by default
-			version_guess = default_version
-			if text != '':
-				# Attempt to guess the version used
-				for version in all_versions:
-					if version.startswith(text):
-						version_guess = version
-						break
+    if partial_version in all_versions:
+        version_guess = partial_version
+    else:
+        # Use a predetermined version by default
+        version_guess = default_version
+        if partial_version != '':
+            # Attempt to guess the version used
+            for version in all_versions:
+                if version.startswith(partial_version):
+                    version_guess = version
+                    break
 
-		return version_guess
+    return version_guess
+
+# Builds Aflred result item as XML
+def get_result_xml(**params):
+    return item_xml.format(**params)
+
+# Retrieves XML document for Alfred results
+def get_result_list_xml(results):
+    xml = '<?xml version="1.0"?>\n<items>\n'
+    for result in results:
+
+        if result.id != None:
+            xml += get_result_xml(
+                id=result.id,
+                url=result.url,
+                title=result.title,
+                subtitle=result.subtitle,
+                valid='yes'
+            )
+
+    xml += '</items>'
+    return xml
+
+# Simplify the format of the query string
+def format_query_str(query_str):
+    # Remove extra whitespace
+    query_str = query_str.strip()
+    query_str = re.sub('\s+', ' ', query_str)
+    # Lowercase query for consistency
+    query_str = query_str.lower()
+    return query_str
 
 # Search the bible for the given book/chapter/verse/version
 def search_bible(query_str):
-	results = []
+    results = []
 
-	# Remove extra whitespace
-	query_str = query_str.strip()
-	query_str = re.sub('(\s)+', ' ', query_str)
-	# Lowercase query for consistency
-	query_str = query_str.lower()
-	# Convert chapter.verse to chapter:verse
-	query_str = re.sub('(\d+)\.(\d+)', '\\1:\\2', query_str)
+    # Create query object for storing query data
+    query = Query()
+    query_str = format_query_str(query_str)
+    # If reference is in form chapter.verse
+    if re.search(chapter_dot_verse_patt, query_str):
+        # Convert chapter.verse to chapter:verse
+        query.separator = '.'
+    else:
+        query.separator = ':'
 
-	# Give the user the option to search the given query (instead of choosing a suggested reference)
-	result = Reference()
-	result.id = 'search-youversion'
-	# Determine if the query includes the translation with which to search
-	version_match = re.findall('( [a-z\d]+)$', query_str)
-	if len(version_match) != 0:
-		version = version_match[0][1:]
-		result.version = guess_version(version)
-	else:
-		# Otherwise, search using the default translation
-		result.version = default_version
-	result.title = 'Search YouVersion for \'' + cgi.escape(query_str) + '\''
-	result.subtitle = result.version.upper() + ' translation'
-	result.url = '{base}?q={query}&amp;version_id={version}'.format(base=search_url, query=urllib.quote_plus(query_str), version=result.version.lower())
+    # Match section of the bible based on query
+    book_matches = re.search(bible_ref_patt, query_str)
 
-	# Prompt user to search the typed query before showing suggestions
-	results.append(result)
+    if book_matches != None:
 
-	# Match section of the bible based on query
-	parts = re.findall(bible_ref_patt, query_str)
+        # Parse partial book name if given
+        if book_matches.group(1) != None and book_matches.group(1) != '':
+            query.book = book_matches.group(1).lower()
+        else:
+            query.book = None
 
-	# 0 = full query
-	# 1 = book name
-	# 2 = leading book number (if any)
-	# 3 = chapter and verse
-	# 4 = chapter
-	# 5 = :verse
-	# 6 = verse
-	# 7 = version
+        # Parse chapter if given
+        if book_matches.group(4) != None and book_matches.group(4) != '':
+            query.chapter = int(book_matches.group(4))
+        else:
+            query.chapter = None
 
-	if parts != []:
+        # Parse verse if given
+        if book_matches.group(7) != None and book_matches.group(7) != '':
+            query.verse = int(book_matches.group(7))
+        else:
+            query.verse = None
 
-		parts = parts[0]
+        # Parse version if given
+        if book_matches.group(8) != None:
+            query.version = book_matches.group(8).lstrip().upper()
+        else:
+            query.version = None
 
-		# Create query object for storing query data
-		query = Query()
+        # Filter book list to match query
+        book_matches = []
+        for book in books:
+            book_name = book.name.lower()
+            # Check if book name begins with the typed book name
+            if book_name.startswith(query.book) or (book_name[0].isnumeric() and book_name[2:].startswith(query.book)):
+                book_matches.append(book)
 
-		# Parse partial book name if given
-		if parts[1] != None and parts[1] != '':
-			query.book = parts[1]
-		else:
-			query.book = None
+        # Build results list from books that matched the query
+        for book in book_matches:
 
-		# Parse chapter if given
-		if parts[4] != None and parts[4] != '':
-			query.chapter = int(parts[4])
-		else:
-			query.chapter = None
+            # Result information
+            result = Reference()
+            result.id = None
 
-		# Parse verse if given
-		if parts[6] != None and parts[6] != '':
-			query.verse = int(parts[6])
-		else:
-			query.verse = None
+            if query.version != None:
+                # Guess version (translation) if possible
+                query.version = guess_version(query.version)
+            else:
+                # Otherwise, use default translation
+                query.version = default_version
 
-		# Parse version if given
-		if parts[7] != None:
-			query.version = parts[7].lstrip().upper()
-		else:
-			query.version = None
+            if query.chapter != None:
 
-		# Filter book list to match query
-		matches = []
-		for book in books:
+                # Find chapter or verse
+                if query.chapter <= book.chapters:
 
-			# Check if book name begins with the typed book name
-			if book.name.lower().startswith(query.book.lower()):
-				matches.append(book)
+                    if query.verse != None:
 
-		# Loop through the books that mathed the query
-		for book in matches:
+                        # Find verse if given
+                        result.id = '{book}.{chapter}.{verse}'.format(
+                            book=book.id,
+                            chapter=query.chapter,
+                            verse=query.verse
+                        )
+                        result.title = '{book} {chapter}{sep}{verse}'.format(
+                            book=book.name,
+                            chapter=query.chapter,
+                            verse=query.verse,
+                            sep=query.separator
+                        )
 
-			# Result information
-			result = Reference()
-			result.id = None
-			result.url = bible_url
+                    else:
 
-			if query.version != None:
-				# Guess translation if possible
-				query.version = guess_version(query.version)
-			else:
-				# Otherwise, use default translation
-				query.version = default_version
+                        # Find chapter if given
+                        result.id = '{book}.{chapter}'.format(
+                            book=book.id,
+                            chapter=query.chapter
+                        )
+                        result.title = '{book} {chapter}'.format(
+                            book=book.name,
+                            chapter=query.chapter
+                        )
 
-			# Append version to URL
-			result.url += '/{version}/'.format(version=query.version.lower())
+            else:
+                # Find book if no chapter or verse is given
 
-			if query.chapter != None:
-				# Find chapter or verse
+                result.id = '{book}.1'.format(book=book.id)
 
-				if query.chapter <= book.chapters:
+                result.title = book.name
 
-					if query.verse != None:
-						# Find verse if given
+            # Create result data using the given information
+            if result.id != None:
+                result.id += '.{version}'.format(version=query.version.lower())
+                result.url = '{base}/{version}/{id}'.format(
+                    base=base_url,
+                    version=query.version.lower(),
+                    id=result.id)
+                result.version = query.version.upper()
+                result.subtitle = '{version} translation'.format(version=result.version)
+                results.append(result)
 
-						result.id = '{book}.{chapter}.{verse}'.format(book=book.id, chapter=query.chapter, verse=query.verse)
+    return get_result_list_xml(results)
 
-						result.title = '{book} {chapter}:{verse}'.format(book=book.name, chapter=query.chapter, verse=query.verse)
-
-					else:
-						# Find chapter if given
-
-						result.id = '{book}.{chapter}'.format(book=book.id, chapter=query.chapter)
-
-						result.title = '{book} {chapter}'.format(book=book.name, chapter=query.chapter)
-
-			else:
-				# Find book if no chapter or verse is given
-
-				result.id = '{book}.1'.format(book=book.id)
-
-				result.title = book.name
-
-			# Create result data using the given information
-			if result.id != None:
-				result.id += '.{version}'.format(version=query.version.lower())
-				result.url += result.id
-				result.version = query.version.upper()
-				result.subtitle = '{version} translation'.format(version=result.version)
-				results.append(result)
-
-	xml = '<?xml version="1.0"?>\n<items>\n'
-	for result in results:
-
-		if result.id != None:
-			xml += """
-			<item uid='{id}' arg='{url}'>
-				<title>{title}</title>
-				<subtitle>{subtitle}</subtitle>
-				<icon>icon.png</icon>
-			</item>
-			""".format(**result.__dict__)
-
-	xml += '\n</items>'
-	return xml
-
-print(search_bible("{query}"), end='')
+print search_bible("{query}")
