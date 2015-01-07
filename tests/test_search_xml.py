20823fdb7d6f2d2421c38f868334fc8276c141a6
Caleb Evans <caleb@calebevans.me>
2015-01-07 12:22:27 -0800

    Revise XML structure test

--- tests/test_search_xml.py
+++ tests/test_search_xml.py
@@ -15,19 +15,27 @@ class SearchXmlTestCase(unittest.TestCase):
         except ET.ParseError:
             self.fail('result list XML is not valid')
 
-    def test_search_xml_attrs(self):
-        '''should match the attributes of each result in the result list'''
+    def test_search_xml_structure(self):
+        '''should contain necessary elements/attributes/values'''
         results = yvs.get_result_list('matthew 6:34')
         result = results[0]
         xml = yvs.get_result_list_xml(results)
         root = ET.fromstring(xml)
+        self.assertEqual(root.tag, 'items', 'root element is incorrectly named')
         item = root.find('item')
-        title = item.find('title')
-        subtitle = item.find('subtitle')
+        self.assertIsNotNone(item, '<item> element is missing')
         self.assertEqual(item.get('uid'), result.uid)
         self.assertEqual(item.get('arg'), result.arg)
+        self.assertEqual(item.get('valid'), 'yes')
+        title = item.find('title')
+        self.assertIsNotNone(title, '<title> element is missing')
         self.assertEqual(title.text, result.title)
+        subtitle = item.find('subtitle')
+        self.assertIsNotNone(subtitle, '<subtitle> element is missing')
         self.assertEqual(subtitle.text, result.subtitle)
+        icon = item.find('icon')
+        self.assertIsNotNone(icon, '<icon> element is missing')
+        self.assertEqual(icon.text, 'icon.png')
 
 if __name__ == '__main__':
     unittest.main()
