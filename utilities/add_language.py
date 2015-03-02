d8215544343e56d91e676112f6fa541562905d00
Caleb Evans <caleb@calebevans.me>
2015-03-01 17:27:54 -0800

    Finish 'add language' script

--- utilities/add_language.py
+++ utilities/add_language.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This script is a handy (albeit imperfect) tool for automatically adding
+# support for any language to YouVersion Suggest. Please see the example
+# function calls below for details on using it.
+
 from __future__ import unicode_literals
 from pyquery import PyQuery as pq
+import os
 import re
 import io
 import json
@@ -11,9 +16,14 @@ import urllib2
 
 json_params = {
     'indent': 2,
+    'separators': (',', ': '),
     'ensure_ascii': False,
-    'sort_keys': False
+    'sort_keys': True
 }
+
+
+def get_url_content(url, **kw):
+    return urllib2.urlopen(url).read().decode('utf-8')
 
 
 def language_matches(text, language):
@@ -29,12 +39,15 @@ def get_version(version_elem):
     patt = '(?<=/versions/)(\d+)-([a-z]+\d*)'
     matches = re.search(patt, url, flags=re.UNICODE)
     return {
-        'name': matches.group(2),
+        'name': matches.group(2).upper(),
         'id': int(matches.group(1))
     }
 
 
-def get_version_elems(d, params):
+def get_version_elems(params):
+
+    d = pq(url='https://www.bible.com/versions',
+           opener=get_url_content)
 
     category_elems = d('#main > article > ul > li')
     version_elems = None
@@ -58,19 +71,20 @@ def get_item_id(item):
 
 def get_versions(params):
 
+    print('Retrieving version data...')
+
     versions = []
 
-    d = pq(url='https://www.bible.com/versions')
+    version_elems = get_version_elems(params)
 
-    version_elems = get_version_elems(d, params)
+    if not version_elems:
+        raise RuntimeError('Cannot find the given language. Aborting.')
 
     for version_elem in version_elems:
         version = get_version(version_elem)
-        if (('max_version_id' in params and
-            (version['id'] <= params['max_version_id']) or
-             'max_versions' in params and len(versions) < params['']) or
-            ('max_version_id' not in params and
-             'max_versions' not in params)):
+        if ('max_version_id' in params and
+           (version['id'] <= params['max_version_id']) or
+           ('max_version_id' not in params)):
             versions.append(version)
 
     versions.sort(key=get_item_name)
@@ -85,13 +99,22 @@ def get_book(book_elem):
     }
 
 
-def get_url_content(url, **kw):
-    return urllib2.urlopen(url).read().decode('utf-8')
+def get_chapter_data():
+
+    chapter_data_path = os.path.join('yv_suggest', 'data', 'bible',
+                                     'chapters.json')
+    with open(chapter_data_path, 'r') as chapter_data_file:
+        chapter_data = json.load(chapter_data_file)
+
+    return chapter_data
 
 
 def get_books(params):
 
+    print('Retrieving book data...')
+
     books = []
+    chapter_data = get_chapter_data()
 
     d = pq(url='https://www.bible.com/bible/{}/jhn.1'
            .format(params['default_version']),
@@ -99,9 +122,13 @@ def get_books(params):
 
     book_elems = d('#menu_book_chapter a[data-book]')
 
+    if not book_elems:
+        raise RuntimeError('Cannot retrieve book data. Aborting.')
+
     for book_elem in book_elems:
         book = get_book(book_elem)
-        books.append(book)
+        if book['id'] in chapter_data:
+            books.append(book)
 
     return books
 
@@ -125,35 +152,51 @@ def get_bible_data(params):
 
 def save_bible_data(params):
 
+    language = params['language']
     bible = get_bible_data(params)
-    bible_path = 'yv_suggest/data/bible/language-{}.json'.format(
-        params['language']['id'])
+    bible_path = os.path.join('yv_suggest', 'data', 'bible',
+                              'language-{}.json'.format(language['id']))
     with open(bible_path, 'w') as bible_file:
         json.dump(bible, bible_file, **json_params)
 
 
 def update_language_list(params):
 
-    langs_path = 'yv_suggest/data/languages.json'
+    print('Updating language list...')
+
+    langs_path = os.path.join('yv_suggest', 'data', 'languages.json')
     with io.open(langs_path, 'r+', encoding='utf-8') as langs_file:
         langs = json.load(langs_file)
         if not any(lang['id'] == params['language']['id'] for lang in langs):
             langs.append(params['language'])
             langs.sort(key=get_item_id)
+            json_str = unicode(json.dumps(langs, **json_params))
             langs_file.truncate(0)
             langs_file.seek(0)
-            langs_file.write(unicode(json.dumps(langs, **json_params)))
+            langs_file.write(json_str)
+            langs_file.write('\n')
 
 
 def add_language(params):
 
-    update_language_list(params)
+    print('Adding support for {}...'
+          .format(params['language']['name']))
     save_bible_data(params)
+    update_language_list(params)
+    print('Support for {} has been successfully added.'
+          .format(params['language']['name']))
 
 
-add_language({
-    'language': {
-        'name': 'Nederlands',
-        'id': 'nl'
-    }
-})
+# Example usage
+# add_language({
+#     'language': {
+#         # Name of language must be written in said language
+#         'name': 'Español',
+#         # ISO language code
+#         'id': 'es'
+#     },
+#     # Versions with lower IDs tend to be more popular and less obscure
+#     'max_version_id': 200,
+#     # If you know the default version you want to use, specify its ID
+#     'default_version': 128
+# })
