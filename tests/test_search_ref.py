fc3d7064ced11979f1e5014cfd72d3665064bacd
Caleb Evans <caleb@calebevans.me>
2015-05-31 16:53:03 -0700

    Use patch() decorator instead of context manager

--- tests/test_search_ref.py
+++ tests/test_search_ref.py
@@ -6,25 +6,25 @@ from mock import patch
 import yv_suggest.search_ref as yvs
 
 
-def test_url_open_chapter():
+@patch('yv_suggest.search_ref.webbrowser.open')
+def test_url_open_chapter(wb_open):
     """should attempt to open chapter URL using webbrowser module"""
-    with patch('yv_suggest.search_ref.webbrowser'):
-        yvs.main('59/jhn.3', prefs={})
-        yvs.webbrowser.open.assert_called_once_with(
-                          'https://www.google.com/search?q=John+3+%28ESV%29')
+    yvs.main('59/jhn.3', prefs={})
+    wb_open.assert_called_once_with(
+        'https://www.google.com/search?q=John+3+%28ESV%29')
 
 
-def test_url_open_verse():
+@patch('yv_suggest.search_ref.webbrowser.open')
+def test_url_open_verse(wb_open):
     """should attempt to open verse URL using webbrowser module"""
-    with patch('yv_suggest.search_ref.webbrowser'):
-        yvs.main('59/jhn.3.17', prefs={})
-        yvs.webbrowser.open.assert_called_once_with(
-            'https://www.google.com/search?q=John+3%3A17+%28ESV%29')
+    yvs.main('59/jhn.3.17', prefs={})
+    wb_open.assert_called_once_with(
+        'https://www.google.com/search?q=John+3%3A17+%28ESV%29')
 
 
-def test_url_open_verse_range():
+@patch('yv_suggest.search_ref.webbrowser.open')
+def test_url_open_verse_range(wb_open):
     """should attempt to open verse range URL using webbrowser module"""
-    with patch('yv_suggest.search_ref.webbrowser'):
-        yvs.main('59/jhn.3.16-17', prefs={})
-        yvs.webbrowser.open.assert_called_once_with(
-            'https://www.google.com/search?q=John+3%3A16-17+%28ESV%29')
+    yvs.main('59/jhn.3.16-17', prefs={})
+    wb_open.assert_called_once_with(
+        'https://www.google.com/search?q=John+3%3A16-17+%28ESV%29')
--- tests/test_view_ref.py
+++ tests/test_view_ref.py
@@ -6,9 +6,9 @@ from mock import patch
 import yv_suggest.view_ref as yvs
 
 
-def test_url_open():
+@patch('yv_suggest.view_ref.webbrowser.open')
+def test_url_open(wb_open):
     """should attempt to open URL using webbrowser module"""
-    with patch('yv_suggest.view_ref.webbrowser'):
-        yvs.main('59/jhn.3.17')
-        yvs.webbrowser.open.assert_called_once_with(
-            'https://www.bible.com/bible/59/jhn.3.17')
+    yvs.main('59/jhn.3.17')
+    wb_open.assert_called_once_with(
+        'https://www.bible.com/bible/59/jhn.3.17')
