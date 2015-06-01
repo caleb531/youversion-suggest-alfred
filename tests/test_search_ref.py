1c92b397c60e511afa772f998331b7784c4eb275
Caleb Evans <caleb@calebevans.me>
2015-06-01 12:55:56 -0700

    Use full function name instead of abbreviated name

--- tests/test_search_ref.py
+++ tests/test_search_ref.py
@@ -7,24 +7,24 @@ import yv_suggest.search_ref as yvs
 
 
 @patch('yv_suggest.search_ref.webbrowser.open')
-def test_url_open_chapter(wb_open):
+def test_url_open_chapter(open):
     """should attempt to open chapter URL using webbrowser module"""
     yvs.main('59/jhn.3', prefs={})
-    wb_open.assert_called_once_with(
+    open.assert_called_once_with(
         'https://www.google.com/search?q=John+3+%28ESV%29')
 
 
 @patch('yv_suggest.search_ref.webbrowser.open')
-def test_url_open_verse(wb_open):
+def test_url_open_verse(open):
     """should attempt to open verse URL using webbrowser module"""
     yvs.main('59/jhn.3.17', prefs={})
-    wb_open.assert_called_once_with(
+    open.assert_called_once_with(
         'https://www.google.com/search?q=John+3%3A17+%28ESV%29')
 
 
 @patch('yv_suggest.search_ref.webbrowser.open')
-def test_url_open_verse_range(wb_open):
+def test_url_open_verse_range(open):
     """should attempt to open verse range URL using webbrowser module"""
     yvs.main('59/jhn.3.16-17', prefs={})
-    wb_open.assert_called_once_with(
+    open.assert_called_once_with(
         'https://www.google.com/search?q=John+3%3A16-17+%28ESV%29')
--- tests/test_view_ref.py
+++ tests/test_view_ref.py
@@ -7,8 +7,8 @@ import yv_suggest.view_ref as yvs
 
 
 @patch('yv_suggest.view_ref.webbrowser.open')
-def test_url_open(wb_open):
+def test_url_open(open):
     """should attempt to open URL using webbrowser module"""
     yvs.main('59/jhn.3.17')
-    wb_open.assert_called_once_with(
+    open.assert_called_once_with(
         'https://www.bible.com/bible/59/jhn.3.17')
