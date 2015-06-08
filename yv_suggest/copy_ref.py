b49406b34678fccd41d28c58ebd8932a378274cb
Caleb Evans <caleb@calebevans.me>
2015-06-07 21:38:20 -0700

    Fix Unicode issue; optimize verse concatenation

--- yv_suggest/copy_ref.py
+++ yv_suggest/copy_ref.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import re
 import urllib2
 import shared
@@ -10,20 +9,18 @@ from HTMLParser import HTMLParser
 # Retrieve HTML for reference with the given ID
 def get_ref_html(ref_uid):
     url = 'https://www.bible.com/bible/{}'.format(ref_uid)
-    return urllib2.urlopen(url).read()
+    return urllib2.urlopen(url).read().decode('utf-8')
 
 
 # Parser for reference HTML
 class ReferenceParser(HTMLParser):
 
     level = 0
-    in_block = False
     in_verse = False
     in_content = False
-    block_depth = None
     verse_depth = None
     content_depth = None
-    ref_text = ''
+    ref_parts = []
 
     def handle_starttag(self, tag, attrs):
         attr_dict = dict(attrs)
@@ -31,12 +28,10 @@ class ReferenceParser(HTMLParser):
             self.level += 1
         if 'class' in attr_dict:
             div_class = attr_dict['class']
-            if div_class == 'p' or re.match('q\d+', div_class):
-                self.in_block = True
-                self.block_depth = self.level
-                self.ref_text += '\n'
-            if re.match('s\d+', div_class):
-                self.ref_text += '\n'
+            if div_class == 'p':
+                self.ref_parts.append('\n\n')
+            if re.match('q\d+', div_class):
+                self.ref_parts.append('\n')
             if 'verse' in div_class:
                 self.in_verse = True
                 self.verse_depth = self.level
@@ -46,12 +41,10 @@ class ReferenceParser(HTMLParser):
 
     def handle_endtag(self, tag):
         # Determine when certain classes of elements end
-        if self.level == self.block_depth:
-            self.in_block = False
         if self.level == self.verse_depth:
             self.in_verse = False
             # Ensure that a space separates consecutive sentences
-            self.ref_text += ' '
+            self.ref_parts.append(' ')
         if self.level == self.content_depth:
             self.in_content = False
         if tag == 'div' or tag == 'span':
@@ -59,38 +52,42 @@ class ReferenceParser(HTMLParser):
 
     def handle_data(self, content):
         if self.in_verse and self.in_content and content.strip() != '':
-            self.ref_text += content
+            self.ref_parts.append(content)
 
     def handle_charref(self, name):
         if self.in_verse and self.in_content:
             if name[0] == 'x':
                 # Handle hexadecimal character references
-                self.ref_text += unichr(int(name[1:], 16))
+                self.ref_parts.append(unichr(int(name[1:], 16)))
             else:
                 # Handle decimal character references
-                self.ref_text += unichr(int(name))
+                self.ref_parts.append(unichr(int(name)))
 
 
 # Parse actual reference content from reference HTML
-def get_ref_text(html):
+def get_ref_text(ref_uid, html):
     parser = ReferenceParser()
     parser.feed(html)
-    return format_ref_text(parser.ref_text)
+    ref_text = format_ref_text(''.join(parser.ref_parts))
+    ref_text = '\n\n' + ref_text
+    ref_text = shared.get_full_ref(shared.get_ref_object(ref_uid)) + ref_text
+    return ref_text
 
 
 def format_ref_text(ref_text):
-    # Collapse spaces
+    # Collapse consecutive spaces to single space
     ref_text = re.sub(' +', ' ', ref_text)
-    # Strip any leading or trailing whitespace
+    # Collapse sequences of three or more newlines into two
+    ref_text = re.sub('\n{2,}', '\n\n', ref_text)
+    # Strip leading/trailing whitespace for entire reference
     ref_text = re.sub('(^\s+)|(\s+$)', '', ref_text)
+    # Strip leading/trailing whitespace for each paragraph
+    ref_text = re.sub('(\n +)|( +\n)', '\n', ref_text)
     return ref_text
 
 
 def main(ref_uid):
-    ref_text = shared.get_full_ref(shared.get_ref_object(ref_uid))
-    ref_text += '\n\n'
-    ref_text += get_ref_text(get_ref_html(ref_uid))
-    print(ref_text)
+    print(get_ref_text(ref_uid, get_ref_html(ref_uid)))
 
 if __name__ == '__main__':
     main('{query}')
