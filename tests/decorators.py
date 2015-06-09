9e3c700816c32f449eb305620665bc877a919330
Caleb Evans <caleb@calebevans.me>
2015-06-09 11:38:43 -0700

    Rewrite all context managers as decorators

---
+++ tests/decorators.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from io import BytesIO
+import sys
+import yv_suggest.shared as yvs
+from functools import wraps
+
+
+def redirect_stdout(fn):
+    """temporarily redirect stdout to new output stream"""
+    @wraps(fn)
+    def wrapper():
+        original_stdout = sys.stdout
+        out = BytesIO()
+        try:
+            sys.stdout = out
+            fn(out)
+        finally:
+            sys.stdout = original_stdout
+    return wrapper
+
+
+def use_prefs(prefs):
+    """temporarily use the given preferences"""
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper():
+            original_prefs = yvs.get_prefs()
+            try:
+                yvs.update_prefs(prefs)
+                fn()
+            finally:
+                yvs.update_prefs(original_prefs)
+        return wrapper
+    return decorator
+
+
+def use_default_prefs(fn):
+    """temporarily use the default values for all preferences"""
+    @wraps(fn)
+    def wrapper():
+        original_prefs = yvs.get_prefs()
+        try:
+            yvs.update_prefs(yvs.get_defaults())
+            fn()
+        finally:
+            yvs.update_prefs(original_prefs)
+    return wrapper
--- tests/test_set_pref.py
+++ tests/test_set_pref.py
@@ -2,25 +2,25 @@
 
 import nose.tools as nose
 import yv_suggest.set_pref as yvs
-import context_managers as ctx
+from decorators import use_default_prefs
 
 
+@use_default_prefs
 def test_set_language():
     """should set preferred language"""
-    with ctx.use_default_prefs():
-        new_language = 'es'
-        yvs.main('language:{}'.format(new_language))
-        prefs = yvs.shared.get_prefs()
-        nose.assert_equal(prefs['language'], new_language)
-        bible = yvs.shared.get_bible_data(prefs['language'])
-        nose.assert_equal(prefs['version'], bible['default_version'])
+    new_language = 'es'
+    yvs.main('language:{}'.format(new_language))
+    prefs = yvs.shared.get_prefs()
+    nose.assert_equal(prefs['language'], new_language)
+    bible = yvs.shared.get_bible_data(prefs['language'])
+    nose.assert_equal(prefs['version'], bible['default_version'])
 
 
+@use_default_prefs
 def test_set_version():
     """should set preferred version"""
-    with ctx.use_default_prefs():
-        bible = yvs.shared.get_bible_data('en')
-        new_version = 59
-        yvs.main('version:{}'.format(new_version))
-        prefs = yvs.shared.get_prefs()
-        nose.assert_equal(prefs['version'], new_version)
+    bible = yvs.shared.get_bible_data('en')
+    new_version = 59
+    yvs.main('version:{}'.format(new_version))
+    prefs = yvs.shared.get_prefs()
+    nose.assert_equal(prefs['version'], new_version)
