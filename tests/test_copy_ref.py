5a55408b4b765e58b90d849fe677171a252fc47c
Caleb Evans <caleb@calebevans.me>
2015-06-09 09:36:26 -0700

    Rewrite 'copy ref' unit tests to be more atomic

--- tests/test_copy_ref.py
+++ tests/test_copy_ref.py
@@ -1,64 +1,106 @@
 #!/usr/bin/env python
 
+from __future__ import unicode_literals
 import nose.tools as nose
 import yv_suggest.copy_ref as yvs
 import context_managers as ctx
 from mock import patch, mock_open, Mock
 
 
-with open('tests/files/psa.23.html') as html_file:
-    TEST_REF_HTML = html_file.read()
-
-
-def before_each():
+with open('tests/files/psa.23.html') as file:
     yvs.urllib2.urlopen = Mock()
-    yvs.urllib2.urlopen.return_value.read = Mock(return_value=TEST_REF_HTML)
+    yvs.urllib2.urlopen.return_value.read = Mock(return_value=file.read())
 
 
-def after_each():
-    pass
-
-
-@nose.with_setup(before_each, after_each)
 def test_copy_chapter():
     '''should copy reference content for chapter'''
-    with open('tests/files/psa.23.txt') as text_file:
-        with ctx.redirect_stdout() as out:
-            yvs.main('111/psa.23')
-            nose.assert_equal(out.getvalue().strip(),
-                              text_file.read().strip())
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23')
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, 'Lorem')
+        nose.assert_regexp_matches(ref_text, 'nunc nulla')
+        nose.assert_regexp_matches(ref_text, 'fermentum')
 
 
-@nose.with_setup(before_each, after_each)
 def test_copy_verse():
     '''should copy reference content for verse'''
-    with open('tests/files/psa.23.2.txt') as text_file:
-        with ctx.redirect_stdout() as out:
-            yvs.main('111/psa.23.2')
-            nose.assert_equal(out.getvalue().strip(),
-                              text_file.read().strip())
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23.2')
+        ref_text = out.getvalue()
+        nose.assert_not_regexp_matches(ref_text, 'Lorem')
+        nose.assert_regexp_matches(ref_text, 'nunc nulla')
+        nose.assert_not_regexp_matches(ref_text, 'fermentum')
 
 
-@nose.with_setup(before_each, after_each)
 def test_copy_verse_range():
     '''should copy reference content for verse range'''
-    with open('tests/files/psa.23.1-2.txt') as text_file:
-        with ctx.redirect_stdout() as out:
-            yvs.main('111/psa.23.1-2')
-            nose.assert_equal(out.getvalue().strip(),
-                              text_file.read().strip())
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23.1-2')
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, 'Lorem')
+        nose.assert_regexp_matches(ref_text, 'nunc nulla')
+        nose.assert_not_regexp_matches(ref_text, 'fermentum')
 
 
-@nose.with_setup(before_each, after_each)
-def test_language():
-    '''should copy reference content in another language'''
-    with open('tests/files/psa.23.txt') as text_file:
-        with ctx.redirect_stdout() as out:
-            yvs.main('128/psa.23', prefs={
-                'language': 'es'
-            })
-            ref_text = text_file.read().strip()
-            ref_text = ref_text.replace('Psalm', 'Salmos')
-            ref_text = ref_text.replace('NIV', 'NVI')
-            nose.assert_equal(out.getvalue().strip(),
-                              ref_text)
+def test_header():
+    '''should prepend reference header to copied string'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('59/psa.23')
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, '^Psalm 23 \(ESV\)')
+
+
+def test_header_language():
+    '''reference header should reflect chosen language'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('128/psa.23', prefs={
+            'language': 'es'
+        })
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, '^Salmos 23 \(NVI\)')
+
+
+def test_charref_dec():
+    '''should evaluate decimal character references'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23')
+        ref_text = out.getvalue().decode('utf-8')
+        nose.assert_regexp_matches(ref_text, '\u201cLorem ipsum\u201d')
+
+
+def test_charref_hex():
+    '''should evaluate hexadecimal character references'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23')
+        ref_text = out.getvalue().decode('utf-8')
+        nose.assert_regexp_matches(ref_text, '\u203a Nunc sem leo')
+
+
+def test_whitespace_words():
+    '''should preserve whitespace between words'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23')
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, 'adipiscing elit.')
+
+
+def test_whitespace_lines():
+    '''should add line breaks where appropriate'''
+    with ctx.redirect_stdout() as out:
+        yvs.main('111/psa.23')
+        ref_text = out.getvalue()
+        nose.assert_regexp_matches(ref_text, 'Psalm 23 \(NIV\)\n\n\S')
+        nose.assert_regexp_matches(ref_text, 'amet,\nconsectetur')
+        nose.assert_regexp_matches(ref_text, 'elit.\n\nUt')
+        nose.assert_regexp_matches(ref_text, 'erat.\n\n\S')
+        nose.assert_regexp_matches(ref_text, 'nec\n\nfermentum')
+
+
+def test_url_always_chapter():
+    '''should always fetch HTML from chapter URL'''
+    with ctx.redirect_stdout() as out:
+        yvs.urllib2.urlopen.reset_mock()
+        yvs.main('59/psa.23.2')
+        ref_text = out.getvalue()
+        yvs.urllib2.urlopen.assert_called_once_with(
+            'https://www.bible.com/bible/59/psa.23')
