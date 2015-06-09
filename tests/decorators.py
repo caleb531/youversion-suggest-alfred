f47d6b970e6e7ef7d34a5f04e14a37777890c3d1
Caleb Evans <caleb@calebevans.me>
2015-06-09 12:54:02 -0700

    Ensure that decorators pass original function args

--- tests/decorators.py
+++ tests/decorators.py
@@ -9,12 +9,12 @@ from functools import wraps
 def redirect_stdout(fn):
     """temporarily redirect stdout to new output stream"""
     @wraps(fn)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         original_stdout = sys.stdout
         out = BytesIO()
         try:
             sys.stdout = out
-            fn(out)
+            fn(out, *args, **kwargs)
         finally:
             sys.stdout = original_stdout
     return wrapper
@@ -24,11 +24,11 @@ def use_prefs(prefs):
     """temporarily use the given preferences"""
     def decorator(fn):
         @wraps(fn)
-        def wrapper():
+        def wrapper(*args, **kwargs):
             original_prefs = yvs.get_prefs()
             try:
                 yvs.update_prefs(prefs)
-                fn()
+                fn(*args, **kwargs)
             finally:
                 yvs.update_prefs(original_prefs)
         return wrapper
@@ -38,11 +38,11 @@ def use_prefs(prefs):
 def use_default_prefs(fn):
     """temporarily use the default values for all preferences"""
     @wraps(fn)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         original_prefs = yvs.get_prefs()
         try:
             yvs.update_prefs(yvs.get_defaults())
-            fn()
+            fn(*args, **kwargs)
         finally:
             yvs.update_prefs(original_prefs)
     return wrapper
