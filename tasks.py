54d35460fd83b3808025d566adeb2473eb01c74e
Caleb Evans <caleb@calebevans.me>
2015-07-10 14:27:42 -0700

    Rewrite invoke tasks

--- tasks.py
+++ tasks.py
@@ -5,11 +5,12 @@ from invoke import task
 
 
 @task
-def test():
-    subprocess.call(['coverage', 'run', '-m', 'nose', '--rednose'])
-
-
-@task
-def cover():
-    subprocess.call(['coverage', 'report'])
-    subprocess.call(['coverage', 'html'])
+def test(cover=False):
+    # Run tests using nose called with coverage
+    code = subprocess.call(['coverage', 'run', '-m', 'nose', '--rednose'])
+    # Also generate coverage reports when --cover flag is given
+    if cover and code == 0:
+        # Add blank line between test report and coverage report
+        print('')
+        subprocess.call(['coverage', 'report'])
+        subprocess.call(['coverage', 'html'])
