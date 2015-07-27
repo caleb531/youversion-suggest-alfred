f90467edaf02ae66cdfd01a34f7e03f20073c12d
Caleb Evans <caleb@calebevans.me>
2015-07-26 21:09:37 -0700

    Create Alfred data dir on test setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,7 +10,6 @@ from mock import patch
 
 yvs.ALFRED_DATA_DIR = os.path.join(tempfile.gettempdir(), 'yvs')
 yvs.PREFS_PATH = os.path.join(yvs.ALFRED_DATA_DIR, 'preferences.json')
-os.mkdir(yvs.ALFRED_DATA_DIR)
 
 
 def mock_open(path, mode):
@@ -23,6 +22,7 @@ patch_open = patch('yvs.shared.open', mock_open, create=True)
 
 
 def setup():
+    os.mkdir(yvs.ALFRED_DATA_DIR)
     patch_open.start()
 
 
