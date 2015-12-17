0813f9b6fbf7b149a83525570916ac1470caf6aa
Caleb Evans <caleb@calebevans.me>
2015-12-16 22:20:56 -0800

    Fix search ref test bugs that cancelled each other out

--- tests/test_search_ref.py
+++ tests/test_search_ref.py
@@ -30,7 +30,7 @@ def test_url_open_verse_range(wb_open):
         'https://www.google.com/search?q=John+3%3A16-17+%28ESV%29')
 
 
-@use_prefs({'searchEngine': 'duckduckgo'})
+@use_prefs({'language': 'en', 'version': 111, 'searchEngine': 'duckduckgo'})
 @patch('webbrowser.open')
 def test_alternate_search_engine(wb_open):
     """should search using alternate search engine in one is chosen"""
@@ -39,7 +39,7 @@ def test_alternate_search_engine(wb_open):
         'https://duckduckgo.com/?q=John+3+%28ESV%29')
 
 
-@use_prefs({'searchEngine': 'xyz'})
+@use_prefs({'language': 'en', 'version': 111, 'searchEngine': 'xyz'})
 @patch('webbrowser.open')
 def test_invalid_search_engine(wb_open):
     """should throw exception if nonexistent web browser is given"""
@@ -47,7 +47,7 @@ def test_invalid_search_engine(wb_open):
         yvs.main('59/jhn.3')
 
 
-@use_prefs({'language': 'es', 'version': 128})
+@use_prefs({'language': 'es', 'version': 128, 'searchEngine': 'google'})
 @patch('webbrowser.open')
 def test_unicode_search(wb_open):
     """should open search URL for reference containing Unicode"""
