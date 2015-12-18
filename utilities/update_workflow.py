09e39154ac246b0f2c923310c2bd0b4cacf0e430
Caleb Evans <caleb@calebevans.me>
2015-12-17 17:59:50 -0800

    Create CORE_PREFS_PATH constant

--- utilities/update_workflow.py
+++ utilities/update_workflow.py
@@ -16,12 +16,15 @@ import shutil
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
-# Filename of the exported workflow
+# Name of the exported workflow file
 WORKFLOW_NAME = 'YouVersion Suggest.alfredworkflow'
 # Path to the current user's home directory
 HOME_DIR = os.path.expanduser('~')
 # Name of Alfred's core preferences file
 CORE_PREFS_NAME = 'com.runningwithcrayons.Alfred-Preferences.plist'
+# Path to Alfred's core preferences file
+CORE_PREFS_PATH = os.path.join(
+    HOME_DIR, 'Library', 'Preferences', CORE_PREFS_NAME)
 # Name of Alfred's user preferences file
 USER_PREFS_NAME = 'Alfred.alfredpreferences'
 # Path to the default location of Alfred's user preferences file
@@ -42,8 +45,7 @@ PKG_RESOURCES = (
 # Get path to directory containing Alfred's user preferences
 def get_user_prefs_dir():
 
-    core_prefs = biplist.readPlist(
-        os.path.join(HOME_DIR, 'Library', 'Preferences', CORE_PREFS_NAME))
+    core_prefs = biplist.readPlist(CORE_PREFS_PATH)
 
     # If user is syncing their preferences using a syncing service
     if 'syncfolder' in core_prefs:
