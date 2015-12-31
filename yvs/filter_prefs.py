4f2d3d195fc90af051e88915f29fdab88068ce59
Caleb Evans <caleb@calebevans.me>
2015-12-31 11:34:33 -0800

    Refactor preference matching

--- yvs/filter_prefs.py
+++ yvs/filter_prefs.py
@@ -33,37 +33,37 @@ def get_pref_defs(user_prefs):
 
 
 # Retrieves Alfred result object for this preference
-def get_pref_result(pref):
+def get_pref_result(pref_def):
 
     return {
-        'title': pref['title'],
-        'subtitle': 'Set your preferred \'{}\''.format(pref['name']),
-        'autocomplete': '{} '.format(pref['key']),
+        'title': pref_def['title'],
+        'subtitle': 'Set your preferred \'{}\''.format(pref_def['name']),
+        'autocomplete': '{} '.format(pref_def['key']),
         'valid': 'no'
     }
 
 
 # Retrieves Alfred result list of all available values for this preference
-def get_value_result_list(user_prefs, pref, query_str):
+def get_value_result_list(user_prefs, pref_def, query_str):
 
-    values = pref['values']()
+    values = pref_def['values']()
     results = []
 
     for value in values:
 
         result = {
-            'arg': '{}:{}'.format(pref['key'], value['id']),
+            'arg': '{}:{}'.format(pref_def['key'], value['id']),
             'title': value['name']
         }
 
-        if value['id'] == user_prefs[pref['key']]:
+        if value['id'] == user_prefs[pref_def['key']]:
             # If this value is the current value, indicate such
             result['subtitle'] = ('This is already your preferred {}'
-                                  .format(pref['name']))
+                                  .format(pref_def['name']))
             result['valid'] = 'no'
         else:
             result['subtitle'] = 'Set this as your preferred {}'.format(
-                pref['name'])
+                pref_def['name'])
 
         # Show all results if query string is empty
         # Otherwise, only show results whose titles begin with query
@@ -89,11 +89,18 @@ def get_pref_matches(query_str):
     return re.search(patt, query_str, flags=re.UNICODE)
 
 
-# Retrieves result list of available preferences, filtered by the given query
-def get_pref_result_list(query_str, prefs):
+# Simplify the given preference key for comparison with a query string
+def format_pref_key(pref_key):
 
-    return [get_pref_result(pref) for pref in
-            prefs if pref['key'].lower().startswith(query_str)]
+    return pref_key.lower()
+
+
+# Retrieves result list of available preferences, filtered by the given query
+def get_pref_result_list(pref_defs, pref_key_query_str=''):
+
+    return [get_pref_result(pref_def) for pref_def in
+            pref_defs if format_pref_key(pref_def['key']).startswith(
+                pref_key_query_str)]
 
 
 # Retrieves result list of preferences or their respective values (depending on
@@ -108,25 +115,25 @@ def get_result_list(query_str):
 
     if pref_matches:
 
-        pref_key = pref_matches.group(1)
-        pref_value = pref_matches.group(2)
+        pref_key_query_str = pref_matches.group(1)
+        pref_value_query_str = pref_matches.group(2)
 
         for pref_def in pref_defs:
             # If key name in query exactly matches a preference key name
-            if pref_def['key'].lower() == pref_key:
+            if format_pref_key(pref_def['key']) == pref_key_query_str:
                 # Get list of available values for the given preference
                 results = get_value_result_list(
-                    user_prefs, pref_def, pref_value)
+                    user_prefs, pref_def, pref_value_query_str)
                 break
         # If no exact matches, filter list of available preferences by query
         if not results:
-            results = get_pref_result_list(query_str, pref_defs)
+            results = get_pref_result_list(pref_defs, pref_key_query_str)
 
     else:
 
         # Should show all available preferences if query is empty
         # or if query does not match
-        results = get_pref_result_list(query_str, pref_defs)
+        results = get_pref_result_list(pref_defs)
 
     # Always sort results by title in this case
     results.sort(key=itemgetter('title'))
