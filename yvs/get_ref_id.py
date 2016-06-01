e823c021b43b8a14c78928398ac1213acdd1f005
Caleb Evans <caleb@calebevans.me>
2016-06-01 15:21:19 -0700

    Rename get_ref_header to get_ref_id

--- yvs/get_ref_id.py
+++ yvs/get_ref_id.py
@@ -1,8 +1,8 @@
-# yvs.get_ref_header
+# yvs.get_ref_id
 # coding=utf-8
 from __future__ import print_function
 import sys
 
 
-# Parse reference header from copied reference content for output
+# Parse reference identifier from copied reference content for output
 print(sys.argv[1].split('\n', 1)[0].rstrip(), end='')
