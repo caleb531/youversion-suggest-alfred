af8cbc0cb2d95bfd72298e092b84749a449bbcbc
Caleb Evans <caleb@calebevans.me>
2016-06-06 13:44:15 -0700

    Move invalid input tests to edge case tests

--- tests/test_filter_refs_edge.py
+++ tests/test_filter_refs_edge.py
@@ -9,6 +9,20 @@ from tests.decorators import use_user_prefs
 
 
 @nose.with_setup(set_up, tear_down)
+def test_empty():
+    """should not match empty input"""
+    results = yvs.get_result_list('')
+    nose.assert_equal(len(results), 0)
+
+
+@nose.with_setup(set_up, tear_down)
+def test_non_alphanumeric():
+    """should not match entirely non-alphanumeric input"""
+    results = yvs.get_result_list('!!!')
+    nose.assert_equal(len(results), 0)
+
+
+@nose.with_setup(set_up, tear_down)
 def test_whitespace():
     """should ignore excessive whitespace"""
     results = yvs.get_result_list('  romans  8  28  a  ')
