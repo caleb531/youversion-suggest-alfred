8653f2c0e63fecd5617dfa063878c846ddafcf97
Caleb Evans <caleb@calebevans.me>
2016-11-06 20:46:00 -0800

    Add additional checks to update_language_list test
    Also make language variable names independent of their actual values.

--- tests/test_add_language/test_update_language_list.py
+++ tests/test_add_language/test_update_language_list.py
@@ -16,15 +16,20 @@ from tests.test_add_language import set_up, tear_down
 @nose.with_setup(set_up, tear_down)
 def test_update_languge_list_add():
     """should add new languages to language list"""
-    kln_language_id = 'kln'
-    kln_language_name = 'Klingon'
-    add_lang.update_language_list(kln_language_id, kln_language_name)
+    new_language_id = 'kln'
+    new_language_name = 'Klingon'
     langs_path = os.path.join(yvs.PACKAGED_DATA_DIR_PATH, 'languages.json')
     with open(langs_path, 'r') as langs_file:
         langs = json.load(langs_file)
-        kln_lang = None
+        orig_num_langs = len(langs)
+    add_lang.update_language_list(new_language_id, new_language_name)
+    with open(langs_path, 'r') as langs_file:
+        langs = json.load(langs_file)
+        num_langs = len(langs)
+        nose.assert_equal(num_langs, orig_num_langs + 1)
+        new_lang = None
         for lang in langs:
-            if lang['id'] == kln_language_id:
-                kln_lang = lang
-        nose.assert_is_not_none(kln_lang)
-        nose.assert_equal(kln_lang['name'], kln_language_name)
+            if lang['id'] == new_language_id:
+                new_lang = lang
+        nose.assert_is_not_none(new_lang)
+        nose.assert_equal(new_lang['name'], new_language_name)
