311c3617612df35ad4a08b719db016ef758c06ae
Caleb Evans <caleb@calebevans.me>
2016-11-07 19:09:53 -0800

    Add additional get_bible_data parameter tests
    Test all cases for default_version and max_version_id parameters.

--- tests/test_add_language/test_get_bible_data.py
+++ tests/test_add_language/test_get_bible_data.py
@@ -10,10 +10,10 @@ import utilities.add_language as add_lang
 from tests.test_add_language import set_up, tear_down
 
 VERSIONS = [
-    {'id': 123, 'name': 'ABC'},
-    {'id': 234, 'name': 'DEF'},
-    {'id': 345, 'name': 'GHI'},
-    {'id': 456, 'name': 'JKL'}
+    {'id': 234, 'name': 'ABC'},
+    {'id': 123, 'name': 'DEF'},
+    {'id': 456, 'name': 'GHI'},
+    {'id': 345, 'name': 'JKL'}
 ]
 BOOKS = [
     {'id': 'gen', 'name': 'Genesis'},
@@ -25,10 +25,37 @@ BOOKS = [
 @nose.with_setup(set_up, tear_down)
 @patch('utilities.add_language.get_books', return_value=BOOKS)
 @patch('utilities.add_language.get_versions', return_value=VERSIONS)
-def test_get_bible_data_default_version(get_versions, get_books):
+def test_get_bible_data_default_version_explicit(get_versions, get_books):
+    """should store explicitly-supplied default version into Bible data"""
     language_id = 'spa'
     default_version = 345
     bible = add_lang.get_bible_data(language_id, default_version)
     get_versions.assert_called_once_with(
         language_id=language_id, max_version_id=None)
+    nose.assert_equal(bible['books'], BOOKS)
     nose.assert_equal(bible['default_version'], default_version)
+    nose.assert_equal(bible['versions'], VERSIONS)
+
+
+@nose.with_setup(set_up, tear_down)
+@patch('utilities.add_language.get_books', return_value=BOOKS)
+@patch('utilities.add_language.get_versions', return_value=VERSIONS)
+def test_get_bible_data_default_version_implicit(get_versions, get_books):
+    """should retrieve implicit default version if none is explicitly given"""
+    bible = add_lang.get_bible_data(language_id='spa')
+    nose.assert_equal(bible['books'], BOOKS)
+    nose.assert_equal(bible['default_version'], 123)
+    nose.assert_equal(bible['versions'], VERSIONS)
+
+
+@nose.with_setup(set_up, tear_down)
+@patch('utilities.add_language.get_books', return_value=BOOKS)
+@patch('utilities.add_language.get_versions', return_value=VERSIONS)
+def test_get_bible_data_max_version_id(get_versions, get_books):
+    """should consider maximum version ID when building Bible data"""
+    language_id = 'spa'
+    bible = add_lang.get_bible_data(language_id, max_version_id=400)
+    nose.assert_equal(bible['books'], BOOKS)
+    nose.assert_equal(bible['default_version'], 123)
+    get_versions.assert_called_once_with(
+        language_id=language_id, max_version_id=400)
