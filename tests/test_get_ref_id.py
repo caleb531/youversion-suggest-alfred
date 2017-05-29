254b272da0c1af6688f99eb6d48af5268c0b8b83
Caleb Evans <caleb@calebevans.me>
2017-05-29 13:16:08 -0700

    Add test for yvs.get_ref_id

---
+++ tests/test_get_ref_id.py
@@ -0,0 +1,19 @@
+# tests.test_get_ref_id
+# coding=utf-8
+
+from __future__ import unicode_literals
+
+import nose.tools as nose
+
+import yvs.get_ref_id as yvs
+from tests import set_up, tear_down
+from tests.decorators import redirect_stdout
+
+
+@nose.with_setup(set_up, tear_down)
+@redirect_stdout
+def test_get_ref_id(out):
+    """should get ID for copied reference"""
+    yvs.main('Psalm 23 (NIV)\nLorem ipsum dolor sit amet')
+    output = out.getvalue().strip()
+    nose.assert_equal(output, 'Psalm 23 (NIV)')
--- yvs/get_ref_id.py
+++ yvs/get_ref_id.py
@@ -1,8 +1,15 @@
 # yvs.get_ref_id
 # coding=utf-8
-from __future__ import print_function
+
+from __future__ import print_function, unicode_literals
 
 import sys
 
-# Parse reference identifier from copied reference content for output
-print(sys.argv[1].split('\n', 1)[0].rstrip(), end='')
+
+def main(ref_content):
+    print(ref_content.split('\n', 1)[0].rstrip().encode('utf-8'),
+          end=''.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1].decode('utf-8'))
