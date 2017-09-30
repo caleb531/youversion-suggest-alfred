caleb531/youversion-suggest-alfred

commit 224c8ae70757a30dbb99d4f5637e96414ec7e538

Rename data/languages to data/bible

utilities/language_parser.py
@@ -14,7 +14,7 @@ class LanguageParser(YVParser):
     # Associates the given language ID with this parser instance
     def __init__(self, language_id):
         YVParser.__init__(self)
-        self.language_url_suffix = '/languages/{}'.format(language_id)
+        self.language_url_suffix = '/bible/{}'.format(language_id)
 
     # Resets parser variables (implicitly called on instantiation)
     def reset(self):

utilities/version_parser.py
@@ -61,7 +61,7 @@ class VersionParser(YVParser):
 def get_versions(language_id):
 
     page_html = shared.get_url_content(
-        'https://www.bible.com/languages/{}'.format(language_id))
+        'https://www.bible.com/bible/{}'.format(language_id))
 
     parser = VersionParser()
     parser.feed(page_html)

tests/html/languages.html
@@ -4,23 +4,23 @@
     <ul>
         <li>
             <span class="lang_code">eng</span>
-            <span class="lang"><a href="/languages/eng">English</a></span>
+            <span class="lang"><a href="/bible/eng">English</a></span>
         </li>
         <li>
             <span class="lang_code">spa</span>
-            <span class="lang"><a href="/languages/spa">Español - Spanish</a></span>
+            <span class="lang"><a href="/bible/spa">Español - Spanish</a></span>
         </li>
         <li>
             <span class="lang_code">spa_es</span>
-            <span class="lang"><a href="/languages/spa_es">Español (España) - Spanish (Spain)</a></span>
+            <span class="lang"><a href="/bible/spa_es">Español (España) - Spanish (Spain)</a></span>
         </li>
         <li>
             <span class="lang_code">nhd</span>
-            <span class="lang"><a href="/languages/nhd">Avañe&#x27;ẽ - Chiripá</a></span>
+            <span class="lang"><a href="/bible/nhd">Avañe&#x27;ẽ - Chiripá</a></span>
         </li>
         <li>
             <span class="lang_code">fra</span>
-            <span class="lang"><a href="/languages/fra">Français - French</a></span>
+            <span class="lang"><a href="/bible/fra">Français - French</a></span>
         </li>
     </ul>
 </article>

tests/test_add_language/test_get_versions.py
@@ -62,7 +62,7 @@ def test_get_versions_url(get_url_content):
     language_id = 'nld'
     add_lang.get_versions(language_id)
     get_url_content.assert_called_once_with(
-        'https://www.bible.com/languages/{}'.format(language_id))
+        'https://www.bible.com/bible/{}'.format(language_id))
 
 
 @nose.with_setup(set_up, tear_down)

tests/test_add_language/test_save_bible_data.py
@@ -25,7 +25,7 @@ BIBLE = {
 def test_save_bible_data_new():
     """should save Bible data to new data file if it doesn't exist"""
     bible_file_path = os.path.join(
-        yvs.PACKAGED_DATA_DIR_PATH, 'languages',
+        yvs.PACKAGED_DATA_DIR_PATH, 'bible',
         'language-{}.json'.format(LANGUAGE_ID))
     add_lang.save_bible_data(language_id=LANGUAGE_ID, bible=BIBLE)
     nose.assert_true(os.path.exists(bible_file_path))
@@ -38,7 +38,7 @@ def test_save_bible_data_new():
 def test_save_bible_data_existing():
     """should update Bible data in existing data file"""
     bible_file_path = os.path.join(
-        yvs.PACKAGED_DATA_DIR_PATH, 'languages',
+        yvs.PACKAGED_DATA_DIR_PATH, 'bible',
         'language-{}.json'.format(LANGUAGE_ID))
     with open(bible_file_path, 'w') as bible_file:
         json.dump(BIBLE, bible_file)

tests/test_compliance.py
@@ -36,10 +36,10 @@ def test_complexity():
 def test_json():
     """All JSON files should comply with the respective schemas"""
     schemas = {
-        'schema-languages': 'yvs/data/languages/languages.json',
+        'schema-languages': 'yvs/data/bible/languages.json',
         'schema-defaults': 'yvs/data/preferences/defaults.json',
-        'schema-chapters': 'yvs/data/languages/chapters.json',
-        'schema-bible': 'yvs/data/languages/language-*.json'
+        'schema-chapters': 'yvs/data/bible/chapters.json',
+        'schema-bible': 'yvs/data/bible/language-*.json'
     }
     for schema_name, data_path_pattern in schemas.iteritems():
         schema_path = 'tests/schemas/{}.json'.format(schema_name)
@@ -77,11 +77,11 @@ def test_import_order():
 
 def test_language_id_correspondence():
     """Language IDs in language.json should have a corresponding data file"""
-    with open('yvs/data/languages/languages.json', 'r') as languages_file:
+    with open('yvs/data/bible/languages.json', 'r') as languages_file:
         languages = json.load(languages_file)
     for language in languages:
         nose.assert_true(
             os.path.exists(os.path.join(
                 'yvs', 'data',
-                'languages', 'language-{}.json'.format(language['id']))),
+                'bible', 'language-{}.json'.format(language['id']))),
             'language-{}.json does not exist'.format(language['id']))

tests/test_filter_prefs.py
@@ -18,7 +18,7 @@ def test_show_languages():
     """should show all languages if no value is given"""
     results = yvs.get_result_list('language')
     nose.assert_equal(
-        len(results), len(glob.glob('yvs/data/languages/language-*.json')))
+        len(results), len(glob.glob('yvs/data/bible/language-*.json')))
 
 
 @nose.with_setup(set_up, tear_down)

tests/test_update_languages.py
@@ -21,7 +21,7 @@ def test_update_languages(out, update_language):
     update_langs.update_languages()
     nose.assert_equal(
         update_language.call_count,
-        len(glob.glob('yvs/data/languages/language-*.json')))
+        len(glob.glob('yvs/data/bible/language-*.json')))
 
 
 @nose.with_setup(set_up, tear_down)

commit 5da546c2ba99bfae71e46b84c747df289efa2983

Correct language URLs from bible/ to language/
The mistake fixed was caused by a regression in 224c8ae.

utilities/language_parser.py
@@ -14,7 +14,7 @@ class LanguageParser(YVParser):
     # Associates the given language ID with this parser instance
     def __init__(self, language_id):
         YVParser.__init__(self)
-        self.language_url_suffix = '/bible/{}'.format(language_id)
+        self.language_url_suffix = '/languages/{}'.format(language_id)
 
     # Resets parser variables (implicitly called on instantiation)
     def reset(self):

utilities/version_parser.py
@@ -61,7 +61,7 @@ class VersionParser(YVParser):
 def get_versions(language_id):
 
     page_html = shared.get_url_content(
-        'https://www.bible.com/bible/{}'.format(language_id))
+        'https://www.bible.com/languages/{}'.format(language_id))
 
     parser = VersionParser()
     parser.feed(page_html)

tests/html/languages.html
@@ -4,23 +4,23 @@
     <ul>
         <li>
             <span class="lang_code">eng</span>
-            <span class="lang"><a href="/bible/eng">English</a></span>
+            <span class="lang"><a href="/languages/eng">English</a></span>
         </li>
         <li>
             <span class="lang_code">spa</span>
-            <span class="lang"><a href="/bible/spa">Español - Spanish</a></span>
+            <span class="lang"><a href="/languages/spa">Español - Spanish</a></span>
         </li>
         <li>
             <span class="lang_code">spa_es</span>
-            <span class="lang"><a href="/bible/spa_es">Español (España) - Spanish (Spain)</a></span>
+            <span class="lang"><a href="/languages/spa_es">Español (España) - Spanish (Spain)</a></span>
         </li>
         <li>
             <span class="lang_code">nhd</span>
-            <span class="lang"><a href="/bible/nhd">Avañe&#x27;ẽ - Chiripá</a></span>
+            <span class="lang"><a href="/languages/nhd">Avañe&#x27;ẽ - Chiripá</a></span>
         </li>
         <li>
             <span class="lang_code">fra</span>
-            <span class="lang"><a href="/bible/fra">Français - French</a></span>
+            <span class="lang"><a href="/languages/fra">Français - French</a></span>
         </li>
     </ul>
 </article>

tests/test_add_language/test_get_versions.py
@@ -62,7 +62,7 @@ def test_get_versions_url(get_url_content):
     language_id = 'nld'
     add_lang.get_versions(language_id)
     get_url_content.assert_called_once_with(
-        'https://www.bible.com/bible/{}'.format(language_id))
+        'https://www.bible.com/languages/{}'.format(language_id))
 
 
 @nose.with_setup(set_up, tear_down)