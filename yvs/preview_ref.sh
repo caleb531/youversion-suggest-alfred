bf614e2225e41eed2550797766aa6cd7acd99065
Caleb Evans <caleb@calebevans.me>
2024-03-23 15:01:27 -0700

    Finish incomplete comment in preview_ref shell script

--- yvs/preview_ref.sh
+++ yvs/preview_ref.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# The Alfred 5.5 Text View can only execute
+# The Alfred 5.5 Text View can only run a executable script as input; it cannot
+# execute arbitrary script code, which we ultimately need to do so we can run
+# the yvs.preview_ref as a module; therefore, this intermediate shell script is
+# necessary to run yvs.preview_ref in the proper module context
 /usr/bin/python3 -m yvs.preview_ref "$@"
