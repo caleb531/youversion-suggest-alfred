e5bb401cd31278b5a6336876234dce4e9bf3352f
Caleb Evans <caleb@calebevans.me>
2024-04-05 12:54:46 -0700

    Fix typo in preview_ref.sh comment

--- yvs/preview_ref.sh
+++ yvs/preview_ref.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# The Alfred 5.5 Text View can only run a executable script as input; it cannot
+# The Alfred 5.5 Text View can only run an executable script as input; it cannot
 # execute arbitrary script code, which we ultimately need to do so we can run
 # the yvs.preview_ref as a module; therefore, this intermediate shell script is
 # necessary to run yvs.preview_ref in the proper module context
